27bc573dc673e881a3e22b5e65a656eb61ecc06e
pravin <admin@gmail.com>
2024-01-22 15:24:02 +0530

    22_01_2024

--- admin_app/models.py
+++ admin_app/models.py
@@ -2,5 +2,6 @@ from django.db import models
 class PyamentStatusModel(models.Model):
     Device_id=models.CharField(max_length=200)
     Status=models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Creation Date")
 
 # Create your models here.
